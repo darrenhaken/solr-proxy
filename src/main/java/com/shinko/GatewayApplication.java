01de7c2f918ebb9ed893a4c326a3832c1611e1da
Darren Haken <darrenhaken@gmail.com>
2017-03-09 15:21:17 +0000

    Only allow GET requests to be proxy, all other HTTP methods are blocked

--- src/main/java/com/shinko/GatewayApplication.java
+++ src/main/java/com/shinko/GatewayApplication.java
@@ -3,11 +3,15 @@ package com.shinko;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @EnableAutoConfiguration
 @EnableZuulProxy
+@Configuration
+@ComponentScan
 public class GatewayApplication {
 
     public static void main(String[] args) throws Exception {
